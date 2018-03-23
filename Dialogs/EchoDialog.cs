50fc952bad220843ab414f6a01a58fec3fa23158
Swagat Mishra <swagatm@microsoft.com>
2018-03-23 11:46:59 -0700

    first change

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                await context.PostAsync($"{this.count++}: You said {message.Text}");
+                await context.PostAsync($"{this.count++}: You said {message.Text} and this code has changed once");
                 context.Wait(MessageReceivedAsync);
             }
         }
